4ee95852e918e98df4aa4bfb69a905f236faf06d
Mykhailenko <juliam4433@gmail.com>
2022-06-27 16:11:17 +0200

    task 10 and 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,15 @@
 const itemEl = document.querySelectorAll('.item');
-let countItemEl = itemEl.length;
-console.log('Number of categories:', countItemEl);
+// let countItemEl = itemEl.length;
+console.log('Number of categories:', itemEl.length);
 
-for (let i = 0; i < itemEl.length; i += 1){
-    console.log('Category:', itemEl[i].firstElementChild.textContent);
-    console.log('Elements:', itemEl[i].lastElementChild.children.length);
-}
+// for (let i = 0; i < itemEl.length; i += 1){
+//     console.log('Category:', itemEl[i].firstElementChild.textContent, 'Elements:', itemEl[i].lastElementChild.children.length);
+// }
 
+const ulEl = document.querySelectorAll('#categories>li');
+ulEl.forEach(el => {
 
+  console.log(
+    `Category: ${el.firstElementChild.textContent}, Elements: ${el.lastElementChild.children.length}`,
+  );
+});
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,11 +1,45 @@
+
+const refs = {
+  inputEl: document.querySelector('[type="number"]'),
+  btnCreate: document.querySelector('[data-create]'),
+  btnDestroy: document.querySelector('[data-destroy]'),
+  divEl : document.querySelector('[id="boxes"]'),
+};
+
+refs.btnCreate.addEventListener('click', btnCreateClick);
+refs.btnDestroy.addEventListener('click', destroyBoxes);
+
+function btnCreateClick() {
+  let amount = + refs.inputEl.value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let basicSize = 30;
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = basicSize + i * 10;
+    const newDivEl = document.createElement('div');
+
+    // newDivEl.style.cssText = `width: ${size}px; height: ${size}px;`;
+    newDivEl.style.height = size + 'px';
+    newDivEl.style.width = size + 'px';
+    newDivEl.style.backgroundColor = getRandomHexColor();
+    newDivEl.style.margin = 5 + 'px';
+    newDivEl.classList.add(".newBox")
+    
+    fragment.appendChild(newDivEl);
+
+  }
+  refs.divEl.appendChild(fragment);
+  console.log(refs.divEl);
+}
+
+function destroyBoxes() {
+    refs.divEl.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const inputEl = document.querySelector('[type="number"]');
-const btnCreate = document.querySelector('[data-create]');
-const btnDestroy = document.querySelector('[data-destroy]');
-
-console.log(inputEl);
-console.log(btnCreate);
-console.log(btnDestroy);
